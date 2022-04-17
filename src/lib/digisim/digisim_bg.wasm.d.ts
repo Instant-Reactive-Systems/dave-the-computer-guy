768143567e67498d0ddf3c6fe8f2ac0d2e0a8dc0
Michael Scofield <mscofield0@tutanota.com>
2022-04-17 23:33:16 +0200

    Added digisim into simulation worker

---
+++ src/lib/digisim/digisim_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_simulation_free(a: number): void;
+export function simulation_new(a: number): number;
+export function simulation_tick(a: number): void;
+export function simulation_init(a: number): void;
+export function simulation_circuit_state(a: number): number;
+export function simulation_set_circuit(a: number, b: number): void;
+export function simulation_set_registry(a: number, b: number): void;
+export function simulation_update_registry(a: number, b: number): void;
+export function simulation_insert_input_event(a: number, b: number, c: number): void;
+export function __wbg_config_free(a: number): void;
+export function __wbg_get_config_max_delay(a: number): number;
+export function __wbg_set_config_max_delay(a: number, b: number): void;
+export function config_new(a: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
