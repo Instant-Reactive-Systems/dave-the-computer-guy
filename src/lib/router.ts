a98ab92e28422d13fc8e976c5db43c7f60b33e55
XSpazzayX <andrijamilovac1@gmail.com>
2022-05-19 21:38:39 +0200

    changed home route in router.ts

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,7 +1,7 @@
 export class Router {
     static readonly SHOP_ROUTE =  "electronics-shop";
     static readonly HANDBOOK_ROUTE ="handbook";
-    static readonly HOME_ROUTE = "home";
+    static readonly HOME_ROUTE = "/";
     static readonly OFFICE_ROUTE = "office";
     static readonly QUESTS_PANEL_ROUTE = "quests-panel";
     static readonly SHOP_PANEL_ROUTE = "shop-panel";
